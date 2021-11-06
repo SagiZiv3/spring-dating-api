9bce89b3521a3e6c96bb5b7513b0ed38dfa0f557
Sagi Ziv <sagi.ziv@s.afeka.ac.il>
2021-11-06 14:35:55 +0200

    Updated imports

--- src/main/java/com/example/demo/controllers/InstanceController.java
+++ src/main/java/com/example/demo/controllers/InstanceController.java
@@ -3,7 +3,11 @@ package com.example.demo.controllers;
 import com.example.demo.boundaries.InstanceBoundary;
 import com.example.demo.boundaries.UserBoundary;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -3,7 +3,11 @@ package com.example.demo.controllers;
 import com.example.demo.boundaries.NewUserBoundary;
 import com.example.demo.boundaries.UserBoundary;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
