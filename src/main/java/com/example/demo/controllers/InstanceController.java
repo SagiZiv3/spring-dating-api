1eef0f55ba09e3ade28aa0b295ddeff8889379ee
Shahar Raz <SRazstudent@gmail.com>
2021-11-06 13:12:34 +0200

    Shahar, Testing

--- src/main/java/com/example/demo/controllers/InstanceController.java
+++ src/main/java/com/example/demo/controllers/InstanceController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+// test upload 
 @RestController
 public class InstanceController {
 
