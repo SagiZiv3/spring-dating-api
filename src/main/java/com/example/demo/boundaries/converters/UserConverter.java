abbbc3615edac0aad8916255fbff208ada310015
Sagi Ziv <sagi.ziv@s.afeka.ac.il>
2021-11-17 19:41:42 +0200

    Added a converter from UserEntity to UserBoundary (and vice-versa)

---
+++ src/main/java/com/example/demo/boundaries/converters/UserConverter.java
@@ -0,0 +1,51 @@
+package com.example.demo.boundaries.converters;
+
+import com.example.demo.boundaries.UserBoundary;
+import com.example.demo.boundaries.UserId;
+import com.example.demo.boundaries.UserRole;
+import com.example.demo.entities.UserEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserConverter {
+    public UserBoundary toUserBoundary(UserEntity userEntity) {
+        UserBoundary boundary = new UserBoundary();
+        boundary.setUserId(toUserIdBoundary(userEntity.getId()));
+        boundary.setAvatar(userEntity.getAvatar());
+        boundary.setRole(toUserRoleBoundary(userEntity.getRole()));
+        boundary.setUsername(userEntity.getUsername());
+        return boundary;
+    }
+
+    public UserId toUserIdBoundary(String userIdString) {
+        String[] values = userIdString.split(";");
+        return new UserId(values[0], values[1]);
+    }
+
+    public UserRole toUserRoleBoundary(String userRole) {
+        if (userRole == null)
+            return null;
+
+        return UserRole.valueOf(userRole);
+    }
+
+    public UserEntity toUserEntity(UserBoundary userBoundary) {
+        UserEntity entity = new UserEntity();
+        entity.setAvatar(userBoundary.getAvatar());
+        entity.setUsername(userBoundary.getUsername());
+        entity.setId(toUserIdEntity(userBoundary.getUserId()));
+        entity.setRole(toUserRoleEntity(userBoundary.getRole()));
+        return entity;
+    }
+
+    public String toUserIdEntity(UserId userId) {
+        return String.format("%s;%s", userId.getDomain(), userId.getEmail());
+    }
+
+    private String toUserRoleEntity(UserRole userRole) {
+        if (userRole == null)
+            return null;
+
+        return userRole.name();
+    }
+}
