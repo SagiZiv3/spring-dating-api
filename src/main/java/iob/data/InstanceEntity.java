d033a448235db72fdad2fa10d85d8d27af956fec
Sagi Ziv <sagi.ziv@s.afeka.ac.il>
2021-11-25 00:12:49 +0200

    Created new service for instances that uses JPA to store the data in a database

--- src/main/java/iob/data/InstanceEntity.java
+++ src/main/java/iob/data/InstanceEntity.java
@@ -1,11 +1,16 @@
 package iob.data;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.util.Date;
 import java.util.Map;
 
@@ -15,13 +20,24 @@ import java.util.Map;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name="INSTANCES")
 public class InstanceEntity {
+    @Id
     private String instanceId;
     private String type;
     private String name;
+    @Getter(AccessLevel.NONE)
     private boolean active;
+    @Transient
     private Date createdTimestamp;
+    @Transient
     private Map<String, String> createdBy;
     private String location;
+    @Transient
     private Map<String, Object> instanceAttributes;
+
+    public boolean getActive(){
+        return this.active;
+    }
 }
--- src/test/java/iob/DatingApiApplicationTests.java
+++ src/test/java/iob/DatingApiApplicationTests.java
@@ -1,10 +1,130 @@
 package iob;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import javax.annotation.PostConstruct;
 
-@SpringBootTest
-class DatingApiApplicationTests {
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
+
+import iob.boundaries.InstanceBoundary;
+import iob.boundaries.helpers.Instance;
+import iob.boundaries.helpers.Location;
+import iob.boundaries.helpers.ObjectId;
+import lombok.Cleanup;
+
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class InstancesAPITests {
+	
+	@Value("${spring.application.name:dummy}")
+	private String domainName;
+	private int port;
+	
+	// setup helper object to invoke HTTP requests
+	private RestTemplate client;
+	
+	// setup a String used to represent the URL used to access the server
+	private String url;
+	
+	// get random port used by server
+	@LocalServerPort
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	@PostConstruct
+	public void initTestCase() {
+		this.url = "http://localhost:" + this.port + "/iob";
+		this.client = new RestTemplate();
+	}
+
+	@Test
+	public void testInsertNewInstance(){
+		// GIVEN 
+		// the server is up ( DO Nothing)
+		InstanceBoundary requestsOutput = addDummyInstance("Sagi Ziv", "MahrozetBliMashmaut");
+		ObjectId id = requestsOutput.getInstanceId();
+		
+		// THEN
+		// instance would be added
+		InstanceBoundary fetchedFromDB = this.client.getForObject(this.url + "/instances/2022a.tomer/user@gmail.com/" + id.getDomain() + "/" + id.getId(),
+								InstanceBoundary.class,
+								id.getDomain() + ";" + id.getId());
+		
+		assertEquals(fetchedFromDB.getName(), "Sagi Ziv");
+		assertEquals(fetchedFromDB.getType(), "MahrozetBliMashmaut");
+
+	}
+
+	private InstanceBoundary addDummyInstance(String name, String type){
+		InstanceBoundary instanceMoveMe = new InstanceBoundary();
+		instanceMoveMe.setName(name);
+		instanceMoveMe.setType(type);
+		InstanceBoundary requestsOutput = this.client.postForObject(this.url + "/instances/2022a.tomer/user@gmail.com",
+																	instanceMoveMe,
+																	InstanceBoundary.class);
+																
+		return requestsOutput;
+	}
+
+	@Test
+	public void testModifyInstance(){
+		InstanceBoundary requestsOutput = addDummyInstance("Sagi Ziv", "MahrozetBliMashmaut");
+		ObjectId id = requestsOutput.getInstanceId();
+		
+		InstanceBoundary newUpdatedInstance = new InstanceBoundary();
+		newUpdatedInstance.setType("MashuAher");
+		Location locObj = new Location(5.0,5.0);
+		newUpdatedInstance.setLocation(locObj);
+
+		// Modifying to new instace
+		this.client.put(this.url + "/instances/2022a.tomer/user@gmail.com/" + id.getDomain() + "/" + id.getId(),
+						newUpdatedInstance,
+						id.getDomain() + ";" + id.getId());
+
+		
+		// Fetching modified instance
+		InstanceBoundary fetchedFromDB = this.client.getForObject(this.url + "/instances/2022a.tomer/user@gmail.com/" + id.getDomain() + "/" + id.getId(),
+																InstanceBoundary.class,
+																id.getDomain() + ";" + id.getId());
+
+		assertEquals(fetchedFromDB.getLocation(), locObj);
+		assertEquals(fetchedFromDB.getType(), "MashuAher");
+	}
+		
+	@Test
+	public void testDeleteAllInstance(){
+		// GIVEN 
+		// we have instances in the program
+		addDummyInstance("Sagi Ziv", "MahrozetBliMashmaut");
+		addDummyInstance("Shahar Raz", "MahrozetBliMashmaut");
+		addDummyInstance("Ben Koren", "MahrozetBliMashmaut");
+		
+		// Delete All Instance
+		this.client.delete(url + "/admin/instances/2022a.tomer/user@gmail.com");
+
+		// Get all instances
+		InstanceBoundary[] returnedFromRequest = this.client.getForObject(url + "/instances/2022a.tomer/user@gmail.com",
+																			InstanceBoundary[].class);
+		
+		assertThat(returnedFromRequest).isEmpty();
+
+	}
+
+	@AfterEach
+	public void cleanup(){
+		this.client.delete(url + "/admin/instances/2022a.tomer/user@gmail.com");
+	}
 
 	@Test
 	void contextLoads() {
