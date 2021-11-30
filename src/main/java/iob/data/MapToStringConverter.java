067cc9cd415a7a7fd9217b90ff28200e3df82eb0
Sagi Ziv <sagi.ziv@s.afeka.ac.il>
2021-11-30 18:40:05 +0200

    Added MapToString converter
    Signed-off-by: Sagi Ziv <sagi.ziv@s.afeka.ac.il>

---
+++ src/main/java/iob/data/MapToStringConverter.java
@@ -0,0 +1,34 @@
+package iob.data;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.persistence.AttributeConverter;
+import java.util.Map;
+
+public class MapToStringConverter implements AttributeConverter<Map<String, Object>, String> {
+    private final ObjectMapper jackson;
+
+    public MapToStringConverter() {
+        this.jackson = new ObjectMapper();
+    }
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, Object> jsonFromEntity) {
+        try {
+            return this.jackson
+                    .writeValueAsString(jsonFromEntity);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Map<String, Object> convertToEntityAttribute(String stringFromDb) {
+        try {
+            return this.jackson.readValue(stringFromDb, Map.class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
