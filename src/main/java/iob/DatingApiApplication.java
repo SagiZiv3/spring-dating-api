76425375563b01515417df090b44c48777f02f9b
Sagi Ziv <sagi.ziv@s.afeka.ac.il>
2021-12-25 13:25:46 +0200

    Added Cors config
    Signed-off-by: Sagi Ziv <sagi.ziv@s.afeka.ac.il>

--- src/main/java/iob/DatingApiApplication.java
+++ src/main/java/iob/DatingApiApplication.java
@@ -1,13 +1,27 @@
 package iob;
 
+import lombok.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DatingApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DatingApiApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
 }
