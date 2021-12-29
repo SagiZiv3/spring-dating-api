958fa11b5b83869ea5b5fec4a28b621c2141d5eb
Sagi Ziv <sagi.ziv@s.afeka.ac.il>
2021-12-29 21:33:59 +0200

    Added new activity for testings and tested invoking activity (both known type and unknown type)
    Signed-off-by: Sagi Ziv <sagi.ziv@s.afeka.ac.il>

--- src/test/java/iob/ActivityApiTests.java
+++ src/test/java/iob/ActivityApiTests.java
@@ -1,10 +1,21 @@
 package iob;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import iob.boundaries.ActivityBoundary;
 import iob.boundaries.InstanceBoundary;
 import iob.boundaries.UserBoundary;
+import iob.boundaries.helpers.InstanceIdWrapper;
+import iob.boundaries.helpers.InvokedByBoundary;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpStatusCodeException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -12,6 +23,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class ActivityApiTests extends AbstractTestClass {
     private InstanceBoundary instanceBoundary;
     private UserBoundary playerBoundary;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public ActivityApiTests(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     @BeforeEach
     public void createInstance() {
@@ -20,60 +37,82 @@ public class ActivityApiTests extends AbstractTestClass {
         playerBoundary = super.createUser(UserRole.PLAYER);
     }
 
-//    @Test
-//    public void testInsertActivity() {
-//        // Create activity boundary
-//        ActivityBoundary activityBoundary = new ActivityBoundary();
-//        activityBoundary.setInstance(new InstanceIdWrapper(instanceBoundary.getInstanceId()));
-//        activityBoundary.setInvokedBy(new InvokedByBoundary(playerBoundary.getUserId()));
-//        activityBoundary.setType("SomeCoolType- ActivityApiTest");
-//
-//        // post activity ( Get it back with the ID)
-//        activityBoundary = this.client.postForObject(super.buildUrl(KEYS.ROOT_NAME),
-//                activityBoundary,
-//                ActivityBoundary.class);
-//
-//        // Export all activities in domain
-//        ActivityBoundary[] returnedFromRequest = this.client.getForObject(super.buildAdminUrl(KEYS.ROOT_NAME, domainName, UserRole.ADMIN.getEmail()),
-//                ActivityBoundary[].class);
-//
-//        // assert addMe is within the activities.
-//        assertThat(returnedFromRequest).contains(activityBoundary);
-//        /*
-//        CRUD:
-//            POST
-//        URL:
-//            http://localhost:8091/iob/activities
-//
-//        Content-Type: (expected object):
-//            application/json
-//
-//        Accept (returns object):
-//
-//        BODY:
-//                {
-//            "type":"demoActivityType",
-//            "instance":{
-//            "instanceId":{
-//            "domain":"2022a.demo",
-//            "id":"352"
-//            }
-//            },
-//            "invokedBy":{
-//            "userId":{
-//            "domain":"2022a.demo",
-//            "email":"user3@demo.com"
-//            }
-//            },
-//            "activityAttributes":{
-//            "key1":"can be set to any value you wish",
-//            "key2":{
-//            "key2Subkey1":"can be nested json"
-//            }
-//            }
-//            }
-//         */
-//    }
+    @Test
+    public void testInsertActivity() {
+        // Create activity boundary
+        ActivityBoundary activityBoundary = new ActivityBoundary();
+        activityBoundary.setInstance(new InstanceIdWrapper(instanceBoundary.getInstanceId()));
+        activityBoundary.setInvokedBy(new InvokedByBoundary(playerBoundary.getUserId()));
+        activityBoundary.setType("testInvokeActivity");
+
+        // post activity ( Get it back with the ID)
+        Object result = this.client.postForObject(super.buildUrl(KEYS.ROOT_NAME),
+                activityBoundary,
+                Object.class);
+
+        assertThat(result).isNotNull();
+
+        // Export all activities in domain
+        ActivityBoundary[] returnedFromRequest = this.client.getForObject(super.buildAdminUrl(KEYS.ROOT_NAME, domainName, UserRole.ADMIN.getEmail()),
+                ActivityBoundary[].class);
+
+        assertThat(returnedFromRequest).isNotNull();
+        assertThat(returnedFromRequest.length).isEqualTo(1);
+        /*
+        CRUD:
+            POST
+        URL:
+            http://localhost:8091/iob/activities
+
+        Content-Type: (expected object):
+            application/json
+
+        Accept (returns object):
+
+        BODY:
+                {
+            "type":"demoActivityType",
+            "instance":{
+            "instanceId":{
+            "domain":"2022a.demo",
+            "id":"352"
+            }
+            },
+            "invokedBy":{
+            "userId":{
+            "domain":"2022a.demo",
+            "email":"user3@demo.com"
+            }
+            },
+            "activityAttributes":{
+            "key1":"can be set to any value you wish",
+            "key2":{
+            "key2Subkey1":"can be nested json"
+            }
+            }
+            }
+         */
+    }
+
+    @Test
+    public void testInsertActivityFailsBecauseUnknownType() {
+        // Create activity boundary
+        ActivityBoundary activityBoundary = new ActivityBoundary();
+        activityBoundary.setInstance(new InstanceIdWrapper(instanceBoundary.getInstanceId()));
+        activityBoundary.setInvokedBy(new InvokedByBoundary(playerBoundary.getUserId()));
+        activityBoundary.setType("sOmE rAnDoM tYpE");
+
+        // post activity ( Get it back with the ID)
+        try {
+            this.client.postForEntity(super.buildUrl(KEYS.ROOT_NAME),
+                    activityBoundary,
+                    Object.class);
+
+            fail("Error was not thrown for unknown type");
+        } catch (HttpStatusCodeException e) {
+            assertThat(e.getStatusCode()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
 
 
     // Enable access from everywhere using: UserAPITests.KEYS.{___}
